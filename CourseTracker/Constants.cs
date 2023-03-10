8eea859bcd3b3c705185549dc3a17ad27db01da9
Andrew <asilkro@me.com>
2023-03-10 12:22:02 -0800

    Revert "Add view flyout pages"
    This reverts commit 3b35b22a9afd2c6f0446e416be511ecf3e13f7ce.

--- CourseTracker/Constants.cs
+++ CourseTracker/Constants.cs
@@ -12,11 +12,11 @@ namespace CourseTracker
 
         public const SQLite.SQLiteOpenFlags Flags =
             // open the database in read/write mode
-            SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.ReadWrite |
             // create the database if it doesn't exist
-            SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.Create |
             // enable multi-threaded database access
-            SQLiteOpenFlags.SharedCache;
+            SQLite.SQLiteOpenFlags.SharedCache;
 
 
         public static string DatabasePath
